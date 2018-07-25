a4798fc2106ec127136331bc81e7100c42bd7272
Anurag Pande <anurag.pande@veritas.com>
2018-07-24 21:25:19 -0700

    Finished chap6

--- code/ds-algo-python/chapter6/C-6.20.py
+++ code/ds-algo-python/chapter6/C-6.20.py
@@ -1,31 +1,26 @@
-def permute(nums, i=0, ans=[], used=[]):
-    if not nums:
-        print(ans)
-        return
-    for n in nums:
-        if n not in used:
-            ans.append(n)
-            used.append(n)
-        permute(nums, i+1, ans, used)
-        ans.pop()
-        used.pop()
+def permuteiter(nums):
+    s=[("".join(nums),0)]
+    while s:
+        nums, l = s.pop()
+        if l == len(nums):
+            print(nums)
+        else:
+            nums = list(nums)
+            for r in range(l, len(nums)):
+                nums[l], nums[r] = nums[r], nums[l]
+                s.append(("".join(nums), l+1))
+                nums[r], nums[l] = nums[l], nums[r]
 
-def permutes(s, ans):
-    if not s:
-        print ans
-    for c in s:
-        ans+=c
-        permute(s, ans)
-
-def permuterec(nums,l,r):
+def permuterec(nums,l=0):
     if l==len(nums):
         print(nums)
         return
     for r in range(l,len(nums)):
         nums[l], nums[r] = nums[r], nums[l]
-        permuterec(nums,l+1,r)
+        permuterec(nums,l+1)
         nums[r], nums[l] = nums[l], nums[r]
 
-permuterec("1234",0,3)
-
-#TODO LATER
+permuteiter(list("ABCD"))
+permuterec(list("ABCD"))
+# I basically just converted recursive to iterative.
+# And recursive I looked up.
--- code/ds-algo-python/chapter6/C-6.21.py
+++ code/ds-algo-python/chapter6/C-6.21.py
@@ -1,1 +1,32 @@
-#TODO LATER
+def subets_of_set(s):
+    stk = []
+    i=0
+    max = 0
+    count = 0
+    for a in s:
+        stk.append(([a], i))
+        i+=1
+    while stk:
+        max = max if max > stk.__sizeof__() else stk.__sizeof__()
+        curr = stk.pop()
+        count +=1
+        print(curr[0])
+        if curr[-1] != len(s)-1:
+            for i in range(curr[-1]+1,len(s)):
+                curr[0].append(s[i])
+                stk.append((list(curr[0]),i))
+                curr[0].pop()
+    print(max, count)
+
+subets_of_set([a for a in range(6)])
+
+# Took 45 minutes. The first ~30 minutes were useless.
+# Did my usual thing of thinking how do i do this instead
+# of actually thinking about how to do it. Then the half-
+# hearted tries in my head. This time i moved to paper
+# quickly enough, good. Then the half hearted tried on paper
+# getting a sorta kinda solution's vague idea in my head
+# and immediately trying to code thinking i'd got it.
+# During the last 15 minutes, i listed down an example
+# exhaustively, and actually solidified the idea i had before,
+# and coding took barely 5 minutes.
