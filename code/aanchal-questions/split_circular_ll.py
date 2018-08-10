f8fd3881ec3c01571f5c53d23d0c6ec303200a9c
Anurag Pande <anurag.pande@veritas.com>
2018-08-09 21:25:47 -0700

    First question

---
+++ code/aanchal-questions/split_circular_ll.py
@@ -0,0 +1,26 @@
+def split(head):
+
+    def _headcheck(node):
+        return node is head or node.next is head
+
+    def _break(n1, n2):
+        while n1.next is not n2:
+            n1 = n1.next
+        n1.next = None
+
+    slow = fast = head
+
+    while not _headcheck(fast):
+        fast = fast.next.next
+        slow = slow.next
+
+    h1 = slow.next
+    _break(slow, h1)
+
+    h2 = fast if fast is head else fast.next
+    if h2 is fast:
+        _break(h1, h2)
+    else:
+        fast.next = None
+
+    return h1,h2
